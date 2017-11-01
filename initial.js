4a8cfe3f3f76879753974fef67f28aae1effc3a1
qmoderninfluence <q@moderninfluence.com>
2017-11-01 16:48:58 -0500

    add readme

--- initial.js
+++ initial.js
@@ -1,10 +1,9 @@
 var tokenex = require('./lib/tokenex')("dwNJ1DnQAdT1DNyjbAYB","2844810397387491");
 
 // console.log(tokenex.token.tokenize("4242424242424242","1"));
-tokenex.token.delete("4242428753444242", function(err, success){
+tokenex.token.tokenize("5454545454545454",1, function(err, success){
   if(err){
     console.log("WTF", err.message, err.code);
   }
-
   console.log("(incallback)Status",success);
 });
